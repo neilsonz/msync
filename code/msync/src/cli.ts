5f51f078885903fce62a5c23b0a5b9095c5efa01
Neilson Hughey <neilsonz@gmail.com>
2020-10-16 17:41:43 +1300

    fixes

--- code/msync/src/cli.ts
+++ code/msync/src/cli.ts
@@ -1,6 +1,5 @@
 import { log } from './common';
-import chalk from 'chalk';
 import command from 'command-interface';
 
-log.info(chalk.bgMagenta.black(' MSYNC '));
+log.info(' MSYNC ');
 command(`${__dirname}/**/*.cmd.js`);
