f455e506674c2e8f2b86e405fa676dca575f61eb
Neilson Hughey <neilsonz@gmail.com>
2020-11-12 13:56:37 +1300

    add chalk support

--- code/msync/src/cli.ts
+++ code/msync/src/cli.ts
@@ -1,5 +1,6 @@
-import { log } from './common';
 import command from 'command-interface';
+const chalk = require('chalk');
 
-log.info(' MSYNC ');
+// tslint:disable-next-line: no-console
+console.log(chalk.green('✅🧞 NEILSONZ MSYNC 😸✅'));
 command(`${__dirname}/**/*.cmd.js`);
